c7dd681512d618394a2fc425c728daf8290de73f
Theusboot <emailmartingcf1@gmail.com>
2023-09-17 14:27:45 -0300

    Update text.py
    upload

--- text.py
+++ text.py
@@ -8,8 +8,8 @@ import asyncio
 
 #sessao = 'repassar mensagem'
 
-#api_hash = "e4acdae0f20aa3b9d53f953ab61d3a14"
-#api_id = 5873950
+#api_hash = "e4acdae0fs20aa3b9dff53f953absd61d3a14"
+#api_id = 58729501
 #sessao = "secao nova"
 
 def obter_chats():
@@ -34,23 +34,21 @@ def obter_chats():
 class Projeto:
 
 	def __init__(self):
-
-		self.sessao = "secao nova"
-		self.api_id = 5873950
-		self.api_hash = "e4acdae0f20aa3b9d53f953ab61d3a14"
+		#api_hash = "e4acdae0fs20aa3b9dff53f953absd61d3a14"
+		#api_id = 58729501
+		#sessao = "secao nova"
 
 		self.client = TelegramClient(self.sessao,self.api_id,self.api_hash)
 
 	
 	def main(self):
-		print('chat 1')
+		print('chat 1') #identificando o chat
 		@self.client.on(events.NewMessage(chats=[1001515706428])) # aqui vai o chat que o bot vai ficar pegando info
 		async def enviar_mensagem(event):
 			if event.message.video or (event.message.photo):
 				await self.client.send_message(1001954230141,event.message)
 
-		#self.client.start()
-		#self.client.run_until_disconnected()
+
 
 	def chat_2(self):
 		print('chat 2')
