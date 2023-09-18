b1f0c6f3fe023bc0ac59cb918c422d30413b9b69
Theusboot <emailmartingcf1@gmail.com>
2023-09-17 23:27:27 -0300

    API atualizada

--- text.py
+++ text.py
@@ -30,32 +30,38 @@ def obter_chats():
 
 class Projeto:
 
-	def __init__(self,api_hash,api_id,sessao,id_canal_copiado):
+	def __init__(self,api_hash,api_id,sessao,id_canal_copiado,id_canal_recepetor):
 		
 		self.api_hash = api_hash  #representação do HASH: "e4acdae0fs20aa3b9dff53f953absd61d3a14"
 		self.api_id = api_id #representação do ID: 58721501
 		self.sessao = sessao #representação do ID: secao nova
 		self.id_canal_copiado = id_canal_copiado # id do canal copiado
+		self.id_canal_recepetor = id_canal_recepetor # id do canal que vai receber as msm
+
 		
 		self.client = TelegramClient(self.sessao,self.api_id,self.api_hash)
 
+	def info(self):
+		pass
+		id_ = "1001515706428" # canal que será copiado
+		id_2 = "1001954230141" # canal que vai receber as mensagens 
+
 	
-	def main(self):
+	def mensagem_video_or_image(self):
 		print('chat 1') #identificando o chat
-		@self.client.on(events.NewMessage(chats=[1001515706428])) # aqui vai o chat que o bot vai ficar pegando info
+		@self.client.on(events.NewMessage(chats=[self.id_canal_copiado])) # aqui vai o chat que o bot vai ficar pegando info
 		async def enviar_mensagem(event):
-			if event.message.video or (event.message.photo):
-				await self.client.send_message(1001954230141,event.message)
+			if event.message.video or (event.message.photo): # repassando imagem e vídeo do canal 
+				await self.client.send_message(self.id_canal_recepetor,event.message)
 
 
 
-	def chat_2(self):
+	def mensagem_text(self):
 		print('chat 2') #identificando o chat
-		@self.client.on(events.NewMessage(chats=[1001538470927]))
+		@self.client.on(events.NewMessage(chats=[self.id_canal_copiado]))
 		async def enviar_mensagem(event):
-
-			if event.message.video or (event.message.photo):
-				await self.client.send_message(1001954230141,event.message)
+			if event.message.text: #repassando mensagem de texto
+				await self.client.send_message(self.id_canal_recepetor,event.message.text)
 
 
 	def comercial(self):
@@ -65,12 +71,12 @@ class Projeto:
 		@self.client.on(events.NewMessage)
 		async def enviar_mensagem(event):
 			await asyncio.sleep(50)
-			await self.client.send_message(1001954230141,mensagem)
+			await self.client.send_message(self.id_canal_recepetor,mensagem)
 					
 
 	def inicializador(self):
-		self.main()
-		self.chat_2()
+		self.mensagem_video_or_image()
+		self.mensagem_text()
 		self.comercial()
 		self.client.start()
 		self.client.run_until_disconnected()
